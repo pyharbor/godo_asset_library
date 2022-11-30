f4e3bec77720c708b2cbc13bbb832686e3994cb5
Jacob Sanders <pyharbor@gmail.com>
2022-11-29 20:26:03 -0500

    adding new files

--- files/addons/cordot/entity/manager/BaseDiscordInteractionManager.gd
+++
@@ -1,7 +0,0 @@
-class_name BaseDiscordInteractionManager
-
-func construct_interaction(_data: Dictionary) -> DiscordInteraction:
-	return null
-
-func get_class() -> String:
-	return "BaseDiscordInteractionManager"
--- files/addons/cordot/entity/message/embed/MessageEmbedVideo.gd
+++
@@ -1,6 +0,0 @@
-# Represents a video within a message embed.
-class_name MessageEmbedVideo extends MessageEmbedAttachment
-
-# doc-hide
-func _init(url: String, proxy_url: String, size: Vector2).(url, proxy_url, size) -> void:
-	pass

48b0101bfd13fc5aa2c1bbc4c06ff30ee5d8d5df
Jacob Sanders <pyharbor@gmail.com>
2022-11-29 20:53:50 -0500

    removing disorganized repo

--- GodotRetro-fa63594457e2a96d4e126e15ab6c70fb4abbb6c3/Examples/scrGameManeger.gd
+++
@@ -1,6 +0,0 @@
-extends Node
-
-# Exit
-func _process(delta):
-	if Input.is_action_just_pressed("player_exit"):
-		get_tree().quit()
--- addons/cordot/util/HTTPHeaders.gd
+++
@@ -1,6 +0,0 @@
-class_name HTTPHeaders
-
-const AUTHORIZATION: String = "Authorization"
-const USER_AGENT: String = "User-Agent"
-const CONTENT_TYPE: String = "Content-Type"
-const CONTENT_LENGTH: String = "Content-Length"

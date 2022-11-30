8ee2d5fd7816f82f07695e7ce9a12a73ac98668d
Jacob Sanders <pyharbor@gmail.com>
2022-11-29 21:42:15 -0500

    removing repeat files

--- files2/22/godot-xr-tools/scenes/pointer_demo/objects/keyboard_test_screen.gd
+++
@@ -1,7 +0,0 @@
-extends Node2D
-
-
-func _on_ClearButton_pressed():
-	$TextEdit1.text = ""
-	$TextEdit2.text = ""
-	$TextEdit3.text = ""
--- files2/7/addons/cordot/entity/guild/WelcomeScreen.gd
+++
@@ -1,7 +0,0 @@
-class_name WelcomeScreen extends DiscordEntity
-
-var description: String setget __set
-var channels: Array     setget __set
-
-func __set(_value) -> void:
-	pass
